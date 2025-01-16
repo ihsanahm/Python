c5f30fd1ad7b14e75ae5f03e9f566f98ebfe9da3
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-16 22:39:57 +0500

    changes in the program Hunderd to one

--- hunderednumbersPrint.py
+++ hunderednumbersPrint.py
@@ -1,7 +1,13 @@
-num=1
+# num=1
 
 
-while num<=100:
-    print(num)
-    num+=1
-    
+# while num<=100:
+#     print(num)
+#     num+=1
+
+
+"""This is print in 100 to 1 """
+i=100
+while i>=1:
+    print(i)
+    i-=1
