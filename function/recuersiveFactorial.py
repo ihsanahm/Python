c2bf5f1c59bb3f94f241e97f5345e63681cd84fd
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-19 18:13:41 +0500

    This program calculate the factorial of a number

---
+++ function/recuersiveFactorial.py
@@ -0,0 +1,15 @@
+def factorial(num):
+    if(num==0):
+        return 0
+    if(num ==1):
+        return 1
+    return num*factorial(num-1)
+
+
+
+
+
+num = int( input("Enter the number to calculate the factorial : "))
+factorial=factorial(num)
+print(factorial)
+
