6d27a998ab46b88d4e5822a432f891bd70e5e6f8
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-23 19:22:37 +0500

    Some changes in program

---
+++ oops/newprogram.py
@@ -0,0 +1,41 @@
+
+
+class Student:
+    collage="Sukkur IBA university "
+    def __init__(self):
+        pass
+
+    def previous_semester_CGPA(self,cgpa):
+        pass
+    def subject2_cgpa(self,sub1,hours):
+        pass
+    def subject3_cgpa(self,sub1,hours):
+        pass
+    def subject4_cgpa(self,sub1,hours):
+        pass
+    def subject5_cgpa(self,sub1,hours):
+        pass
+    def subject6_cgpa(self,sub1,hours):
+        pass
+    def subjectPR1_cgpa(self,sub1,hours):
+        pass
+    def subjectPR1_cgpa(self,sub1,hours):
+        pass
+    def subjectPR1_cgpa(self,sub1,hours):
+        pass
+    def get_cgpa_from_other_methods_calculate_final(self):
+        pass
+
+
+    
+student=Student(input("Enter name : "))
+
+        
+
+
+   
+
+
+        
+
+        
