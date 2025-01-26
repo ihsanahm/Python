3cb9eb70d84934259ab256e0ff0ff127b8593fbe
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-26 15:13:46 +0500

    me restarant mangement program using funcations

---
+++ function/restaurant.py
@@ -0,0 +1,32 @@
+
+def Restaurant():
+    menu={
+    "Pizza":200,
+    "Burger":100,
+    "Sandwich":350,
+    "pasta":120,
+    "coffee":80,
+    "Cool Drinks":90
+    }
+    total=0
+    is_true=True
+
+    while is_true:
+        item=input("Enter the name of the item you want to order :")
+        if item in menu:
+            total +=menu[item]
+            print("Item added..")
+        else:
+            print(" This item is not available")
+        item1=input("Do you want more item (Y/N) : ")
+        if item1=='Y':
+            is_true=True
+        else:
+            is_true=False
+    return total
+print("<<<===**** Welcome to our restaurant ****===>>>")
+print("This is our menu selecte your favrouate food :)")
+print("Pizza 🍕🍕 : Rs200\nBurger 🍔🍔 : Rs100\nSandwich 🥪🥪 : Rs350\npasta 🍝🍝 : 120\ncoffee 🍵🍵 : Rs80\nCool Drinks 🍷🍷 : Rs90")
+
+total_bill=Restaurant()
+print(total_bill)
