8c5b42598e8fe072b4f26f6b4c6e5e3c53dd4da2
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-27 17:36:34 +0500

    Project was updated adding a new method of withdraw and handle all exception

--- projectsofPthons/bankingprojectUssin_OOP.py
+++ projectsofPthons/bankingprojectUssin_OOP.py
@@ -1,4 +1,4 @@
-class BelanceException():
+class BelanceException(Exception):
     pass
 
 
@@ -8,9 +8,9 @@ class BankAccount:
     def __init__(self, initialAccount, accName):
         self.belance=initialAccount
         self.name=accName
-        print(
-            f"\nAccount '{self.name}' created.\nbelance =Rs{self.belance:.2f}"
-        )
+        # print(
+        #     f"\nAccount '{self.name}' created.\nbelance =Rs{self.belance:.2f}"
+        # )
     def get_belance(self ):
         print(
             f"\nAccount '{self.name}' created.\nbelance =Rs{self.belance:.2f}"
@@ -19,10 +19,21 @@ class BankAccount:
     def Deposite(self, ammount):
         self.belance = self.belance+ammount
         print("Deposite is completed.")
+        self.get_belance()
     def VariableTransication(self , ammoun):
-        if(ammoun>=self.belance):
+        if(self.belance>=ammoun):
             return
         else:
             raise BelanceException(
                 f" sorry Account '{self.name}' only has a belance '{self.belance:.2f}"
             )
+    def withdraw(self , amount):
+        try:
+            self.VariableTransication(amount)
+            self.belance = self.belance - amount
+            print("Withdraw is completed.")
+            self.get_belance()
+        except BelanceException as error:
+            print(f"\nWithdraw is intrrupted: {error}.")
+
+
