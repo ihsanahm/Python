e23115ced0cef5d6003bc319b78f68864a8a7bc9
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-27 18:27:53 +0500

    Transfer metod is added to the project

--- projectsofPthons/Main.py
+++ projectsofPthons/Main.py
@@ -5,5 +5,6 @@ Attia=BankAccount(2000,"Attia")
 
 Ali.get_belance()
 Attia.get_belance()
-Attia.Deposite(900)
-Ali.withdraw(11000)
+Attia.Deposit(900)
+Ali.withdraw(10)
+Ali.transfer(1000,Attia)
--- projectsofPthons/bankingprojectUssin_OOP.py
+++ projectsofPthons/bankingprojectUssin_OOP.py
@@ -8,20 +8,20 @@ class BankAccount:
     def __init__(self, initialAccount, accName):
         self.belance=initialAccount
         self.name=accName
-        # print(
-        #     f"\nAccount '{self.name}' created.\nbelance =Rs{self.belance:.2f}"
-        # )
-    def get_belance(self ):
         print(
             f"\nAccount '{self.name}' created.\nbelance =Rs{self.belance:.2f}"
         )
+    def get_belance(self ):
+        print(
+            f"\nAccount '{self.name}'\nbelance =Rs{self.belance:.2f}"
+        )
         
-    def Deposite(self, ammount):
-        self.belance = self.belance+ammount
-        print("Deposite is completed.")
+    def Deposit(self, amount):
+        self.belance = self.belance+amount
+        print("Deposite is completed. ✅")
         self.get_belance()
-    def VariableTransication(self , ammoun):
-        if(self.belance>=ammoun):
+    def VariableTransication(self , amount):
+        if(self.belance>=amount):
             return
         else:
             raise BelanceException(
@@ -31,9 +31,22 @@ class BankAccount:
         try:
             self.VariableTransication(amount)
             self.belance = self.belance - amount
-            print("Withdraw is completed.")
+            print("Withdraw is completed. ✅")
             self.get_belance()
         except BelanceException as error:
             print(f"\nWithdraw is intrrupted: {error}.")
+    
+    def transfer(self , amount,account):
+        try:
+            print("\n***************\n\nAmount is transfering... 🔃")
+            self.VariableTransication(amount)
+            self.withdraw(amount)
+            account.Deposit(amount)
+            print("\nTransfered succesfully ✅\n\n***************")
+        except BelanceException as error:
+            print(f"\nTransfer is intrrupted!❌.{error}")
+            
+
+
 
 
