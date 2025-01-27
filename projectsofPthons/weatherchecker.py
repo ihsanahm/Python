59fe7f57ca5e57a78ea45fbfa76795274b35fefb
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2025-01-27 23:18:23 +0500

    The forent design of the app was done now

--- projectsofPthons/weatherchecker.py
+++ projectsofPthons/weatherchecker.py
@@ -9,14 +9,33 @@ import pytz
 
 root =Tk()
 root.title("Weather App")
-root.geometry("800x400+250+100")
+root.geometry("900x550+250+20")
 root.resizable(False,False)
 
-search_image=PhotoImage("search.png")
+search_image=PhotoImage( file = "D:\Python repository\projectsofPthons\search.png")
 myimge=Label(image=search_image)
 myimge.place(x=20,y=20)
 
 
+textfield=tk.Entry(root,justify="center",width=18,font=("poppins",26,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=48,y=37)
+
+textfield.focus()
+
+search_Icon=PhotoImage(file="D:\Python repository\projectsofPthons\search_icon.png")
+myIcon=Button(image=search_Icon,borderwidth=0,cursor="hand2",bg="#404040")
+myIcon.place(x=400,y=31.5)
+
+Logo_image=PhotoImage( file = "D:\Python repository\projectsofPthons\logo.png")
+myLogo=Label(image=Logo_image)
+myLogo.place(x=180,y=130)
+
+Button_image = PhotoImage(file="D:\\Python repository\\projectsofPthons\\NewBox.png")
+
+myButton=Label(image=Button_image)
+myButton.pack(padx=5,pady=5,side=BOTTOM)
+
 
 root.mainloop()
+
 
